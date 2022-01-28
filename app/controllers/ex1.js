30eeff2203b310a4f63a084a7b14ef88651a7742
VB <22002397@etu.unicaen.fr>
2022-01-28 14:13:46 +0100

    Edit ex1.hbs, add this.size and add buttons. Change controller for clear and get style, conditionsif size < 20 and between 20 and 50

---
+++ app/controllers/ex1.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Ex1Controller extends Controller {
+  @tracked content = 'Texte par défaut';
+  @tracked info = '';
+  MAX = 100;
+
+  get size() {
+    return this.content.length;
+  }
+
+  get max() {
+    return this.MAX;
+  }
+
+  @action
+  clear() {
+    this.clear;
+  }
+
+  // eslint-disable-next-line getter-return
+  get style() {
+    if (this.size <= 20) {
+      return 'danger';
+    }
+    if (this.size >= 20 && this.size <= 50) {
+      return 'warning';
+    }
+  }
+}
