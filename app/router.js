3bf04fb6083bbd7918378d8a8ec6890abd98b329
VB <22002397@etu.unicaen.fr>
2022-03-09 14:53:02 +0100

    All routes are finished

--- app/router.js
+++ app/router.js
@@ -5,18 +5,12 @@ export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
-
 Router.map(function () {
-  this.route('ex2');
-  this.route('testNew');
-  this.route('contacts', function () {
-    this.route('update');
-  });
   this.route('contacts-update');
-  this.route('categories', function () {
+  this.route('categories',{ path: '/contact/:categoriesl' }), function () {
     this.route('add');
     this.route('update');
-    this.route('contacts', function () {
+    this.route('contacts',{ path: '/contact/:contactsl' }), function () {
       this.route('add');
       this.route('update');
     });
